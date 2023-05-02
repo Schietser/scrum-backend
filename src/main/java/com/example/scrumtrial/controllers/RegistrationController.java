4bd705d9ad9e3bad84f2058e7b3f0061ea24d5b9
Mano <lemmensmano@gmail.com>
2023-05-02 15:13:37 +0200

    oop

--- src/main/java/com/example/scrumtrial/controllers/RegistrationController.java
+++ src/main/java/com/example/scrumtrial/controllers/RegistrationController.java
@@ -6,11 +6,14 @@ import com.twilio.Twilio;
 import com.twilio.rest.verify.v2.Service;
 import com.twilio.rest.verify.v2.service.Verification;
 import com.twilio.rest.verify.v2.service.VerificationCheck;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 //import java.time.ZonedDateTime;
 //import java.util.Objects;
@@ -20,6 +23,7 @@ import java.util.function.Supplier;
 // TODO: CHECK FOR UNIQUENESS OF ACC CREATION IDENTIFIER
 @RestController
 @RequestMapping("/registration")
+@Slf4j
 public class RegistrationController {
     private final Service ssid;
     private final UserService uService;
@@ -40,7 +44,7 @@ public class RegistrationController {
                 .createAsync().get();
     }
 
-    private VerificationCheck checkVerificationCode(String identifier, String code) throws Exception{
+    private VerificationCheck checkVerificationCode(String identifier, String code) {
         return VerificationCheck.creator(
                 ssid.getSid())
                 .setTo(identifier)
@@ -48,13 +52,12 @@ public class RegistrationController {
                 .create();
     }
 
-    public ResponseEntity<LoginReply> an_even_more_suspicious_fun(Object req, Supplier<String> identifier_supp, Supplier<String> code_supp){
+    private LoginReply validateAndSave(Object req, Supplier<String> identifier_supp, Supplier<String> code_supp){
         try {
             vc = checkVerificationCode(identifier_supp.get(), code_supp.get());
         } catch (Exception e){
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body(new LoginReply(false).error(Optional.of(e.getMessage())));
+            log.error(e.getMessage(), e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
         if(vc.getValid()) {
             if(req instanceof CheckNewUserEmail){
@@ -62,49 +65,44 @@ public class RegistrationController {
             } else if (req instanceof CheckNewUserSms) {
                 uService.saveUser((CheckNewUserSms) req);
             } else{
-                System.err.println("there is an unimplemented type ඞ among us ඞ");
-                System.err.println("YOU FORGOR \uD83D\uDC80 TO IMPL TYPE " + req.getClass().getSimpleName());
-                return ResponseEntity.internalServerError().body(new LoginReply(false).error(Optional.of("I forgor \uD83D\uDC80")));
+                log.error("there is an unimplemented type ඞ among us ඞ");
+                log.error("YOU FORGOR \uD83D\uDC80 TO IMPL TYPE " + req.getClass().getSimpleName());
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "I forgor \uD83D\uDC80");
             }
-            return ResponseEntity.ok(new LoginReply(true));
+            return new LoginReply(true);
         }
-        return ResponseEntity.badRequest().body(new LoginReply(false).error(Optional.of("Failed to authenticate")));
+        throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE ,"Failed to authenticate");
     }
 
     @PostMapping("/usr/getCode/email")
-    public ResponseEntity<LoginReply> createUserWithEmail(@RequestBody CreateUserWithEmailReq req){
+    public LoginReply sendVerificationCode(@RequestBody CreateUserWithEmailReq req){
         try {
             sendVerificationCode(req.getEmail(), "email");
-            return ResponseEntity.ok().body(new LoginReply(true));
+            return new LoginReply(true);
         } catch (Exception e){
-            /* TODO: FULL ERROR LOGGING */
-            /* and reply error-logic */
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body(new LoginReply(false).error(Optional.of(e.getMessage())));
+            log.error(e.getMessage(), e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
     }
 
     @PostMapping("/usr/checkCode/email")
-    public ResponseEntity<LoginReply> createUser(@RequestBody CheckNewUserEmail req){
-        return an_even_more_suspicious_fun(req, req::getEmail, req::getCode);
+    public LoginReply createUserIfCodeIsValid(@RequestBody CheckNewUserEmail req){
+        return validateAndSave(req, req::getEmail, req::getCode);
     }
 
     @PostMapping("/usr/getCode/sms")
-    public ResponseEntity<LoginReply> createUserWithSms(@RequestBody CreateUserWithSmsReq req){
+    public ResponseEntity<LoginReply> sendVerificationCode(@RequestBody CreateUserWithSmsReq req){
         try {
             sendVerificationCode(req.getSms(), "sms");
             return ResponseEntity.ok(new LoginReply(true));
         } catch (Exception e){
-            // TODO: FULL ERROR LOGGING
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             return ResponseEntity.badRequest().body(new LoginReply(false).error(Optional.of(e.getMessage())));
         }
     }
 
     @PostMapping("/usr/checkCode/sms")
-    public ResponseEntity<LoginReply> createUser(@RequestBody CheckNewUserSms req){
-        return an_even_more_suspicious_fun(req, req::getSms, req::getCode);
+    public LoginReply createUserIfCodeIsValid(@RequestBody CheckNewUserSms req){
+        return validateAndSave(req, req::getSms, req::getCode);
     }
 }
